d65f3c1eb344507db40efe40551f60c3fe07c00b
Loris Libralato <lorislibra21@gmail.com>
2024-05-16 11:10:45 +0200

    Close #5

---
+++ src/main/java/it/unipd/mtss/RomanPrinter.java
@@ -0,0 +1,18 @@
+////////////////////////////////////////////////////////////////////
+// Loris Libralato 2068237
+// Leonardo Lucato 2075528
+////////////////////////////////////////////////////////////////////
+
+package it.unipd.mtss;
+
+public class RomanPrinter {
+
+    public static String print(int num) {
+        return printAsciiArt(IntegerToRoman.convert(num));
+    }
+
+    private static String printAsciiArt(String romanNumber) {
+        // TODO
+        return null;
+    }
+}
